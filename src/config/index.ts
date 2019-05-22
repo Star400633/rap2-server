41aed3f14ee2f19ece6c31b1c43e2acca88be153
liupanwang <liupanwang@ypshengxian.com>
2019-05-22 11:41:13 +0800

    detete idea local build

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import { IConfigOptions } from "../types";
 // local or development or production
 let configObj: IConfigOptions =
     (process.env.NODE_ENV === 'local' && require('./config.local')).default ||
-    (process.env.NODE_ENV === 'development' && require('./config.prod')).default ||
-    require('./config.dev').default
+    (process.env.NODE_ENV === 'development' && require('./config.dev')).default ||
+    require('./config.prod').default
 
 export default configObj
