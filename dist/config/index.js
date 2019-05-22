e30bc8536a0b4e6bbcc680b0fa7a2a2a851ae004
liupanwang <liupanwang@ypshengxian.com>
2019-05-22 11:07:05 +0800

    gitignore

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,6 +1,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let configObj = (process.env.NODE_ENV === 'local' && require('./config.local')).default ||
-    (process.env.NODE_ENV === 'development' && require('./config.dev')).default ||
-    require('./config.prod').default;
+    (process.env.NODE_ENV === 'development' && require('./config.prod')).default ||
+    require('./config.dev').default;
 exports.default = configObj;
 //# sourceMappingURL=index.js.map
